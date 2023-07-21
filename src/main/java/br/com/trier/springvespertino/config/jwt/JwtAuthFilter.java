9cd6cb6aca8d1cfcfcceaa9221985d2a9d6e0a5c
unknown <vini.tvf@hotmail.com>
2023-07-21 17:32:43 -0300

    Commit home

--- src/main/java/br/com/trier/springvespertino/config/jwt/JwtAuthFilter.java
+++ src/main/java/br/com/trier/springvespertino/config/jwt/JwtAuthFilter.java
@@ -33,6 +33,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 		if (authHeader != null && authHeader.startsWith("Bearer ")) {
 			token = authHeader.substring(7);
 			username = jwtUtil.extractUsername(token);
+		} else {
+			
 		}
 
 		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
